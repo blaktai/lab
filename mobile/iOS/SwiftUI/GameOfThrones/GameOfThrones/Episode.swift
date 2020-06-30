d1b5f74982c4ca796d760d4f1f682bbbd9c5cd76
blaktai <55172595+blaktai@users.noreply.github.com>
2020-06-30 00:57:34 -0400

    Removed initialzer to rely on memberwise initializer

--- mobile/iOS/SwiftUI/GameOfThrones/GameOfThrones/Episode.swift
+++ mobile/iOS/SwiftUI/GameOfThrones/GameOfThrones/Episode.swift
@@ -10,15 +10,6 @@ struct Episode {
     let summary: String
     let mediumImageID: String
     let originalImageID: String
-    init(airdate: String, id: Int, name: String, number: Int, season: Int, runtime: Int, summary: String, mediumImageID: String, originalImageID: String) {
-        self.airdate = airdate
-        self.id = id
-        self.name = name
-        self.number = number
-        self.season = season
-        self.runtime = runtime
-        self.summary = summary
-        self.mediumImageID = mediumImageID
-        self.originalImageID = originalImageID
-    }
-}
+
+  }
+
