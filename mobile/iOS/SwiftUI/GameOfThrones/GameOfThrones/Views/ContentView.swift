5bfe46ae24fb7f7f81bc241852358a2c79f9b889
blaktai <55172595+blaktai@users.noreply.github.com>
2020-07-06 17:41:33 -0400

    Refactored Content View to use EpisodeListView

--- mobile/iOS/SwiftUI/GameOfThrones/GameOfThrones/Views/ContentView.swift
+++ mobile/iOS/SwiftUI/GameOfThrones/GameOfThrones/Views/ContentView.swift
@@ -1,24 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
-  
-  private let episodes = Episode.episodesBySeason
-  
+    
   var body: some View {
     NavigationView {
-      List {
-        ForEach(0..<episodes.count) { seasonIndex in
-          Section(header: Text("Season \(seasonIndex + 1)")) {
-            ForEach(0..<self.episodes[seasonIndex].count) { episodeIndex  in
-              NavigationLink(destination: DetailView( self.episodes[seasonIndex][episodeIndex])) {
-                Cell(self.episodes[seasonIndex][episodeIndex])
-              }
-            }
-          }
-        }
-      }.navigationBarTitle("Game of Thrones")
-    }.navigationBarBackButtonHidden(false)
-  }
+      EpisodeListView(withNavigation: true)
+        .navigationBarTitle("Game of Thrones")
+        .navigationBarBackButtonHidden(false)
+      }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
