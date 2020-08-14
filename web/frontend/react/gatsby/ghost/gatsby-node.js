a1f70c8d22579f680b6fab998d7838940d67c7a2
blaktai <55172595+blaktai@users.noreply.github.com>
2020-08-13 23:46:34 -0400

    update to gatsby node

--- web/frontend/react/gatsby/ghost/gatsby-node.js
+++ web/frontend/react/gatsby/ghost/gatsby-node.js
@@ -1,5 +1,37 @@
 const path = require('path');
 
 exports.createPages = async ({ graphql, actions }) => {
+
+    const postTemplate = path.resolve(`./src/templates/post.js`);
+
     const { createPage } = actions;
+    const result = await graphql(`
+    allGhostPost(filter: {authors: {elemMatch: {name: {eq: "Ghost"}}}}) {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    `);
+    const posts = result.data.allGhostPost.edges;
+    posts.forEach(({post, i}) => {
+        createPage({
+            path: i === 0 ? post : `${post.slug}page/${i + 1}/`,
+            component: postTemplate,
+            context: {
+                // Data passed to context is available
+                // in page queries as GraphQL variables.
+                slug: post.slug,
+                // limit: postsPerPage,
+                // skip: i * postsPerPage,
+                // numberOfPages: numberOfPages,
+                // humanPageNumber: currentPage,
+                // prevPageNumber: prevPageNumber,
+                // nextPageNumber: nextPageNumber,
+                // previousPagePath: previousPagePath,
+                // nextPagePath: nextPagePath,
+            },
+        }) 
+    });
 }
